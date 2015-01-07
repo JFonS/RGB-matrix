67f725ebcf91c737371d3ef2f98f00fb499e4541
JFonS <joan.fonssanchez@gmail.com>
2015-01-07 22:10:32 +0100

    Two windows

--- main.cpp
+++ main.cpp
@@ -6,42 +6,52 @@
 using namespace std;
 using namespace sf;
 
-int main(int argc, char* argv[]) {
+void imageRegion(string path) {
+  const int cellSize = 40;
   Texture tex;
-  tex.loadFromFile("img.jpg");
-  
-  Image img;
-  img.loadFromFile("img.jpg");
+  tex.loadFromFile(path);
 
-  Vector2i scSize = Vector2i(tex.getSize().x, tex.getSize().y);
+  Image img;
+  img.loadFromFile(path);
+
+  Vector2i imgSize = Vector2i(tex.getSize().x, tex.getSize().y);
 
   Sprite sprite;
   sprite.setTexture(tex);
-  RenderWindow window(VideoMode(scSize.x, scSize.y), "RGB", Style::Titlebar | Style::Close);
   
-  RectangleShape rect;
-  rect.setFillColor(Color(0,255,0,50));
-  rect.setOutlineThickness(1);
-  rect.setOutlineColor(Color::Green);
+  RenderWindow windowImg(VideoMode(imgSize.x, imgSize.y), path, Style::Titlebar | Style::Close);
+  RenderWindow windowResult(VideoMode(cellSize * 8, cellSize * 8), "RGM Matrix", Style::Titlebar | Style::Close);
   
+  RectangleShape select;
+  select.setFillColor(Color(0,255,0,50));
+  select.setOutlineThickness(1);
+  select.setOutlineColor(Color::Green);
+  
+  RectangleShape cell;
+  cell.setOutlineThickness(1);
+  cell.setOutlineColor(Color::Black);
+  cell.setSize(Vector2f(cellSize,cellSize));
+
   Color result[8][8];
   
-  Vector2f startPos;
-  bool done  = false; 
-  while (window.isOpen()) {
-    
+  Vector2f startPos(-1,-1);
+
+  while (windowImg.isOpen()) {
     Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == Event::Closed or (event.type == Event::KeyPressed and event.key.code == Keyboard::Escape))  window.close();
-      else if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+    while (windowImg.pollEvent(event)) {
+      if (event.type == Event::Closed or (event.type == Event::KeyPressed and event.key.code == Keyboard::Escape))  {
+        windowImg.close();
+
+      } else if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
         startPos.x = event.mouseButton.x;
         startPos.y = event.mouseButton.y;
-        rect.setPosition(startPos);
-        rect.setSize(Vector2f());
-      }
-      else if (event.type == Event::MouseMoved && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-        int x = min(event.mouseMove.x, scSize.x);
-        int y = min(event.mouseMove.y, scSize.y);
+        select.setPosition(startPos);
+        select.setSize(Vector2f());
+
+      } else if (event.type == Event::MouseMoved && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+
+        int x = min(event.mouseMove.x, imgSize.x);
+        int y = min(event.mouseMove.y, imgSize.y);
         Vector2f offset;
         int absX = abs(startPos.x - x);
         int absY = abs(startPos.y - y); 
@@ -49,11 +59,13 @@ int main(int argc, char* argv[]) {
         else offset = Vector2f(absX, absX);
         if (startPos.x > x) offset.x *= -1;
         if (startPos.y > y) offset.y *= -1;
-        rect.setSize(offset);
-      } else if (event.type == Event::KeyPressed and event.key.code == Keyboard::Return and not done) {
-        int regionSize = rect.getSize().x;
-        int cellSize = regionSize/8;
+        select.setSize(offset);
+      }
 
+      int regionSize = select.getSize().x;
+      int cellSize = regionSize/8;
+      if (cellSize != 0) {
+        //if (cellSize < 0) cellSize *= -1;
         for (int i = 0; i < 8; ++i) {
           for (int j = 0; j < 8; ++j) {
             int r, g, b;
@@ -68,30 +80,32 @@ int main(int argc, char* argv[]) {
             }
             int n = cellSize * cellSize;
             result[i][j] = Color(r/n, g/n, b/n, 255);
-            cout << "(" << result[i][j].r << "," << result[i][j].g << "," << result[i][j].b << ")   ";
           }
-          cout << endl;
-        }
-        done = true;
-      } 
-
-    window.clear();
-    if (not done) {
-      window.draw(sprite);
-      window.draw(rect);
-    } else {
-      int size = 40;
-      rect.setOutlineColor(Color::Black);
-      rect.setSize(Vector2f(size,size));
-      for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-          rect.setFillColor(result[i][j]);
-          rect.setPosition(i*size, j*size);
-          window.draw(rect);
         }
       }
     }
-    window.display();
+
+    windowImg.clear();
+    windowResult.clear();
+
+    windowImg.draw(sprite);
+    windowImg.draw(select);
+
+    for (int i = 0; i < 8; ++i) {
+      for (int j = 0; j < 8; ++j) {
+        cell.setFillColor(result[i][j]);
+        cell.setPosition(i*cellSize, j*cellSize);
+        windowResult.draw(cell);
+      }
+    }
+
+    windowImg.display();
+    windowResult.display();
   }
-  }
+  windowResult.close();
+}
+
+
+int main(int argc, char* argv[]) {
+  imageRegion("img.jpg");
 }
